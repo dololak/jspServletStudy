240074d3527f6be006db6899f9b90da32dcb1dfd
dololak <todaerika@nate.com>
2019-07-26 16:57:17 +0900

    Part API를 이용한 파일 업로드 구현

--- jspServletStudy/src/servlet/FileUploadServlet.java
+++ jspServletStudy/src/servlet/FileUploadServlet.java
@@ -3,21 +3,27 @@ package servlet;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 
+@MultipartConfig(
+        location = "C:\\attaches",
+        maxFileSize = -1,
+        maxRequestSize = -1,
+        fileSizeThreshold = 1024)
 @WebServlet("/fileUpload")
 public class FileUploadServlet extends HttpServlet {
 
     private static final String CHARSET = "utf-8";
     private static final String ATTACHES_DIR = "C:\\attaches";
-    private static final int LIMIT_SIZE_BYTES = 1024 * 1024;
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -26,42 +32,44 @@ public class FileUploadServlet extends HttpServlet {
         request.setCharacterEncoding(CHARSET);
         PrintWriter out = response.getWriter();
 
-//        File attachesDir = new File(ATTACHES_DIR);
-//
-//        DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
-//        fileItemFactory.setRepository(attachesDir);
-//        fileItemFactory.setSizeThreshold(LIMIT_SIZE_BYTES);
-//
-//        ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
-//
-//        try {
-//            List<FileItem> items = fileUpload.parseRequest(request);
-//            for (FileItem item : items) {
-//                if (item.isFormField()) {
-//                    System.out.printf("파라미터 명 : %s, 파라미터 값 : %s \n", item.getFieldName(), item.getString(CHARSET));
-//                } else {
-//                    System.out.printf("파라미터 명 : %s, 파일 명 : %s, 파일 크기 : %s bytes \n", item.getFieldName(),
-//                            item.getName(), item.getSize());
-//
-//                    if (item.getSize() > 0) {
-//                        String separator = File.separator;
-//                        int index = item.getName().lastIndexOf(separator);
-//
-//                        String fileName = item.getName().substring(index + 1);
-//                        File uploadFile = new File(ATTACHES_DIR + separator + fileName);
-//                        item.write(uploadFile);
-//                    }
-//                }
-//
-//            }
-//
-//            out.println("<h1>파일 업로드 완료</h1>");
-//
-//        } catch (Exception e) {
-//            // 파일 업로드 처리 중 오류가 발생하는 경우
-//            e.printStackTrace();
-//            out.println("<h1>파일 업로드 중 오류가 발생하였습니다.</h1>");
-//        }
+        String contentType = request.getContentType();
+
+        if (contentType != null && contentType.toLowerCase().startsWith("multipart/")) {
+            // getParts()를 통해 Body에 넘어온 데이터들을 각각의 Part로 쪼개어 리턴
+            Collection<Part> parts = request.getParts();
+
+            for (Part part : parts) {
+                System.out.printf("파라미터 명 : %s, contentType : %s,  size : %d bytes \n", part.getName(),
+                        part.getContentType(), part.getSize());
+
+                if (part.getHeader("Content-Disposition").contains("filename=")) {
+                    String fileName = extractFileName(part.getHeader("Content-Disposition"));
+                    
+                    if (part.getSize() > 0) {
+                        System.out.printf("업로드 파일 명 : %s \n", fileName);
+                        part.write(ATTACHES_DIR + File.separator + fileName);
+                        part.delete();
+                    }
+
+                } else {
+                    String formValue = request.getParameter(part.getName());
+                    out.printf("name : %s, value : %s <br> \n", part.getName(), formValue);
+                }
+            }
+        } else {
+            out.println("<h1>enctype이 multipart/form-data가 아님</h1>");
+        }
+    }
+
+    private String extractFileName(String partHeader) {
+        for (String cd : partHeader.split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                String fileName = cd.substring(cd.indexOf("=") + 1).trim().replace("\"", "");;
+                int index = fileName.lastIndexOf(File.separator);
+                return fileName.substring(index + 1);
+            }
+        }
+        return null;
     }
 
 }
