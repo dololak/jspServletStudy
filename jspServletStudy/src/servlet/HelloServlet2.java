9756831f4a5d05eb768bf51de339fe5584ee089b
dololak <todaerika@nate.com>
2019-07-31 22:18:46 +0900

    예제 커밋

--- jspServletStudy/src/servlet/HelloServlet2.java
+++ jspServletStudy/src/servlet/HelloServlet2.java
@@ -5,16 +5,12 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet(
-//        name="HelloServlet",
-//        urlPatterns="/hello",
-//        loadOnStartup = 1)
-public class HelloServlet extends HttpServlet {
+//@WebServlet("/HelloServlet")
+public class HelloServlet2 extends HttpServlet {
     
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -24,7 +20,7 @@ public class HelloServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("<h1>Hello World</h1>");
+		out.println("<h1>Hello World2</h1>");
 	}
 
 }
